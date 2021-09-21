f64c4abe70b4cb9fa14c4f06658308d8319b99a5
Mallory <mallorydoescoding@gmail.com>
2021-09-21 16:24:10 -0500

    header added, styling sheet created but no styling done.

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+
+
+
+    return (
+        <div className='headWrap'>
+            <h1>Spacestagram</h1>
+            <p>Brought to you by Mallory Hall</p>
+            <p>Created using NASA's image API's</p>
+        </div>
+    )
+}
+
+export default Header
