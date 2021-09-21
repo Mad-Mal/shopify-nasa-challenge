7c083241221b3bf8c4eba88be6f5aaefca7f68b0
Mallory <mallorydoescoding@gmail.com>
2021-09-21 16:51:48 -0500

    styling done and a few more dependencies added

--- src/components/DataCards.js
+++ src/components/DataCards.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './DataCard.css';
 import LikeBtn from './LikeBtn';
